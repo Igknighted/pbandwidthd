2e6b701b4f3553a80f8f8ff28af35970d1576809
Sam <sam.igknighted@gmail.com>
2015-07-12 18:37:16 -0500

    Update pbandwidthd.pl

--- pbandwidthd.pl
+++ pbandwidthd.pl
@@ -1,15 +1,23 @@
 #!/usr/bin/perl
 
-# pbandwidthd - perl bandwidth processor daemon/tool
+# pbandwidthd - perl bandwidth logger tool
 #
-# This tool will process bandwidth consumption to/from the server and provide
+# This tool will log bandwidth consumption to/from the server and provide
 # useful information as to which IPs and ports are wasting precious network
 # resources.
-#
-# Currently this is only wrapping around tcpdump and sorting some data for use. This doesn't really do anything yet.
-# Will add UDP support soon.
+# 
+# This does not support UDP at this time
 
+my $logdir = "/var/log/pbandwidthd"; # directory we will store log info in
+my $ticks_reset = 100; # this is how many ticks happen before the packet buffer resets
+my $ticks = 0;           # initial tick is 0
 
+my %compound_data; # this is the hash table that stores ip packet statistics
+# %compound_data{ip_address} structure
+# in		- This is the total amount of data sent to the server
+# out		- This is the total amount of data the server set to the ip
+# [0-9]+in	- This is the total amount of data send to the server over a specific port
+# [0-9]+out	- This is the total amount of data send to the ip over a specific port
 
 my $protocol = "NOTSET";
 
@@ -19,9 +27,63 @@ $locals =~ s/\n//g;
 $locals =~ s/ /\|/g;
 $locals =~ s/\./\\./g;
 
+
+# create our log directory if it doesn't exist
+if (! -d $logdir){
+	mkdir($logdir);
+}
+if (! -d $logdir){
+	print "$logdir does not exist.";
+	exit 1;
+}
+
 # Start a TCP dump so we can parse through it to preserve bandwidth data.
-open STDIN, '-|', 'tcpdump -vnni any port 80';
+#open STDIN, '-|', 'tcpdump -vnni any port 80';
+open STDIN, '-|', 'tcpdump -vnni any';
 while ($line = <>) {
+
+	# inside of this mechanism, we will take $compound_data and store the info in files.
+	if($ticks >= $ticks_reset){
+		for (keys %compound_data){
+			my $ipaddr = $_;
+			my $logfile = "$logdir/$ipaddr";
+
+			# read in existing data and add it to current measurements
+			open (MYFILE, "< $logfile");
+			while (<MYFILE>) { chomp;
+				# split the data line
+				my @stored_info = split(": ", $_);
+				# add the shit together
+				$compound_data{$ipaddr}{$stored_info[0]} += $stored_info[1];
+			}
+			# fuck you file!
+			close(MYFILE);
+
+
+			# build our new formated file
+			my $ipaddrdatafile = "";
+			for(keys $compound_data{$ipaddr}){
+				my $field = $_;
+				$ipaddrdatafile =  "$ipaddrdatafile$field: $compound_data{$ipaddr}{$field}\n";
+			}
+
+
+			# write out the new log data
+			open (MYFILE, "> $logfile");
+			print MYFILE "$ipaddrdatafile";
+			close(MYFILE);
+		}
+
+
+		# reset the compound_data table
+		undef %compound_data;
+		my %compound_data;
+
+		# reset the ticks to zero!
+		$ticks=0;
+	}
+
+
 	if($protocol eq "TCP"){
 		# since we're processing TCP, the $line should look like this:
 		#          23.253.224.117.22 > 72.3.128.84.23483: Flags [P.], cksum 0xc340 (incorrect -> 0x3cf5), seq 206785:207377, ack 240, win 316, options [nop,nop,TS val 6277983 ecr 2367950115], length 592
@@ -70,16 +132,35 @@ while ($line = <>) {
 		#
 		# $portb - Service port in use for $ipb, only relevant when $flow is "out"
 
-		print $flow, " ", $ipa, ":", $porta, " -> ", $ipb, ":", $portb, " ... ", $length, "\n";
+		#print $flow, " ", $ipa, ":", $porta, " -> ", $ipb, ":", $portb, " ... ", $length, "\n";
 
-		# TODO:
-		#   We will log the following data...
-		#   - Bandwidth consumption per port over IP
-		#   - Bandwidth consuption per IP address
-		#   - Bandwidth consumption per IP address over port and IP
-		#
-		#   This data should be rotatated daily around midnight or reset.
-		#   Need to make tools to parse the data in a friendly manner (they will be CLI and apache GUI facing).
+		my $ip;
+		my $port;
+
+		# figure out which is the remote IP and port in use
+		if($flow eq "in"){
+			$ip = $ipa;
+			$port = $portb;
+		} elsif($flow eq "out"){
+			$ip = $ipb;
+			$port = $porta;
+		}
+
+		# ensure the IP is in the compound_data hash table
+		if(! exists $compound_data{$ip}){
+			$compound_data{$ip}{"total in"} = 0;
+			$compound_data{$ip}{"total out"} = 0;
+		}
+
+		# ensure port is in hashtable
+		if(! exists $compound_data{$ip}{"$port $flow"}){
+			$compound_data{$ip}{"$port $flow"} = 0;
+		}
+
+
+		# lazy math
+		$compound_data{$ip}{"total $flow"} += $length;
+		$compound_data{$ip}{"$port $flow"} += $length;
 
 	}
 
@@ -97,6 +178,9 @@ while ($line = <>) {
 	}else{
 		# reset the protocol when we process the data it gave us.
 		$protocol = "NOTSET";
+
+		# increase the tick number. This is used to intermittently store bandwidth data.
+		$ticks++;
 	}
 
 }
