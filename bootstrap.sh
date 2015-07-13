2487b8372c8a5feb87d7ee127f0b8832522f9bdf
Sam <sam.igknighted@gmail.com>
2015-07-12 20:44:18 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+
+if [ -d /usr/local/bin ]; then
+  mkdir -p /usr/local/bin
+fi
+
 cd /usr/local/bin
 wget https://raw.githubusercontent.com/Igknighted/pbandwidthd/master/pbandwidthd.pl
-cd /etc/init.d
-wget https://raw.githubusercontent.com/Igknighted/pbandwidthd/master/init.d/pbandwidthd
+chmod +x pbandwidthd.pl
+
+if [ -d /etc/init.d ]; then
+  cd /etc/init.d
+  wget https://raw.githubusercontent.com/Igknighted/pbandwidthd/master/init.d/pbandwidthd
+fi
+
 if [ -d /etc/systemd/system ]; then
   cd /etc/systemd/system
   wget https://raw.githubusercontent.com/Igknighted/pbandwidthd/master/init.d/pbandwidthd.service
